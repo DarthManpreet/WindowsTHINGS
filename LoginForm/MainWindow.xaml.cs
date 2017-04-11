c62e7a0d6efb9cd5610a728163bb80036e1f41c3
Manpreet Bahl <manpreet@pdx.edu>
2017-04-11 13:27:26 -0700

    Login screen foundation

---
+++ LoginForm/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(usernameBox.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a username!", "Error");
+                return;
+            }
+
+            if(passwordBox.Password == String.Empty)
+            {
+                MessageBox.Show("Please enter a password!", "Error");
+                return;
+            }
+
+            MessageBox.Show("You entered:\n\tUsername: " + usernameBox.Text + "\n\tPassword: " + passwordBox.Password, "Success");
+        }
+    }
+}
